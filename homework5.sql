86762b88d46a5050a8f94d72bdf7f32b6663e280
Natsa613 <95077420+Natsa613@users.noreply.github.com>
2022-04-29 00:04:39 +0900

    Add files via upload

---
+++ homework5.sql
@@ -0,0 +1,15 @@
+create database ticket;
+use ticket;
+create table number (
+    name    char(32) character set utf8,
+    kids1   int(3),
+    adult1  int(3),
+    kids2   int(3),
+    adult2  int(3),
+    kids3   int(3),
+    adult3  int(3),
+    kids4   int(3),
+    adult4  int(3),
+    sum     int(3)
+);
+describe number;
